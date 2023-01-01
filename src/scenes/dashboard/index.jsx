7b4932e99326f7475db86d0879d948d7fae33c0b
kreanimator <backin.v@yandex.ru>
2023-01-02 00:13:03 +0200

    Initial commit

---
+++ src/scenes/dashboard/index.jsx
@@ -0,0 +1,13 @@
+import Header from "../../components/Header";
+import { Box } from "@mui/system";
+
+const Dashboard = () => {
+    return <Box m="20px"> 
+    <Box display = "flex" justifyContent="space-between" alignItems= "center">
+        <Header title = "DASHBOARD" subtitle = "Welcome to your dashboard" />
+        </Box> 
+    </Box>
+
+}
+
+export default Dashboard ;
